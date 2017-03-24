38aaf962936a2bbf706ff5d2842473e4d134a3d4
chenaibing <348549708@qq.com>
2017-03-24 14:08:28 +0800

    删除&忽略.vscode，配置中不打包react-dom和动画包

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -32,7 +32,14 @@ module.exports = {
             commonjs2: 'react',
             commonjs: 'react',
             amd: 'react'
-        }
+        },
+        'react-dom':{
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+        },
+        'react-addons-css-transition-group': 'ReactCSSTransitionGroup'
     }],
     module:{
         loaders:[{
