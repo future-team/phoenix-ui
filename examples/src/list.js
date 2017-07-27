c23e574653f5ae7642e3d948ee52c0cc21a0fd3e
chenaibing <348549708@qq.com>
2017-07-27 20:43:38 +0800

    list demo fixed

--- examples/src/list.js
+++ examples/src/list.js
@@ -7,7 +7,10 @@ import Radio from "phoenix-ui/lib/radio"
 import Icon from "phoenix-ui/lib/icon"
 import Switch from "phoenix-ui/lib/switch"
 import Badge from "phoenix-ui/lib/badge"
-import Code from "./code/code";
+import Image from "phoenix-ui/lib/image"
+import Code from "./code/code"
+
+const IMAGE_URL = 'http://img5.duitang.com/uploads/item/201602/11/20160211214958_RhdCY.jpeg'
 
 export default class star extends Component{
     render(){
@@ -15,55 +18,19 @@ export default class star extends Component{
             <div>
                 <h2 className="comp-title">List</h2>
                 <List>
-                    <List.Header>列表标题</List.Header>
+                    <List.Header>标题</List.Header>
+                    <List.Item className="clickable">
+                        <List.Col>
+                            <label>选择一</label>
+                        </List.Col>
+                    </List.Item>
+                </List>
+                <Code target="list-normal" />
+
+                <List>
                     <List.Item>
                         <List.Col>
                             <Input type="text" placeholder="请输入" />
-                        </List.Col>
-                    </List.Item>
-                    <List.Item required>
-                        <List.Col>
-                            <Input type="text" value="请输入姓名" />
-                        </List.Col>
-                    </List.Item>
-                    <List.Item>
-                        <List.Col heading>
-                            <label>姓名</label>
-                        </List.Col>
-                        <List.Col>
-                            <Input type="text" placeholder="请输入" value="一键清除" clear />
-                        </List.Col>
-                    </List.Item>
-                    <List.Item disabled>
-                        <List.Col heading>
-                            <label>姓名</label>
-                        </List.Col>
-                        <List.Col>
-                            <Input type="text" placeholder="请输入" value="disabled" />
-                        </List.Col>
-                    </List.Item>
-                    <List.Item error>
-                        <List.Col heading>
-                            <label>姓名</label>
-                        </List.Col>
-                        <List.Col>
-                            <Input type="text" placeholder="请输入" value="error" clear error/>
-                        </List.Col>
-                    </List.Item>
-                    <List.Item>
-                        <List.Col heading>
-                            <label>密码</label>
-                        </List.Col>
-                        <List.Col>
-                            <Input type="password" placeholder="请输入" value="123456" clear visible />
-                        </List.Col>
-                    </List.Item>
-                    <List.Item>
-                        <List.Col heading>
-                            <label><Icon phIcon="feedback" />反馈</label>
-                        </List.Col>
-                        <List.Col>
-                            <Input type="text" placeholder="请输入" value="123456" />
                         </List.Col>
                     </List.Item>
                     <List.Item>
@@ -81,9 +48,57 @@ export default class star extends Component{
                         </List.Col>
                     </List.Item>
                 </List>
+                <Code target="list-input" />
 
                 <List>
-                    <List.Header>列表标题</List.Header>
+                    <List.Item disabled>
+                        <List.Col heading>
+                            <label>姓名</label>
+                        </List.Col>
+                        <List.Col>
+                            <Input type="text" placeholder="请输入" value="disabled" />
+                        </List.Col>
+                    </List.Item>
+                </List>
+                <Code target="list-disabled" />
+
+                <List>
+                    <List.Item error>
+                        <List.Col heading>
+                            <label>姓名</label>
+                        </List.Col>
+                        <List.Col>
+                            <Input type="text" placeholder="请输入" value="error" clear error/>
+                        </List.Col>
+                    </List.Item>
+                </List>
+                <Code target="list-error" />
+
+                <List>
+                    <List.Item>
+                        <List.Col heading>
+                            <label>密码</label>
+                        </List.Col>
+                        <List.Col>
+                            <Input type="password" placeholder="请输入" value="123456" clear visible />
+                        </List.Col>
+                    </List.Item>
+                </List>
+                <Code target="list-password" />
+
+                <List>
+                    <List.Item>
+                        <List.Col heading>
+                            <label><Icon phIcon="kaidiancankao" />反馈</label>
+                        </List.Col>
+                        <List.Col>
+                            <Input type="text" placeholder="请输入" value="123456" />
+                        </List.Col>
+                    </List.Item>
+                </List>
+                <Code target="list-phicon" />
+
+                <List>
                     <List.Item navigate>
                         <List.Col heading>
                             <label>性别</label>
@@ -92,14 +107,10 @@ export default class star extends Component{
                             <Input type="text" placeholder="男/女" disabled />
                         </List.Col>
                     </List.Item>
-                    <List.Item navigate>
-                        <List.Col heading>
-                            <label>性别</label>
-                        </List.Col>
-                        <List.Col tail>
-                            <Input type="text" value="男" disabled />
-                        </List.Col>
-                    </List.Item>
+                </List>
+                <Code target="list-navigate" />
+
+                <List>
                     <List.Item>
                         <List.Col heading>
                             <label>Female</label>
@@ -112,7 +123,7 @@ export default class star extends Component{
                         <List.Col heading>
                             <label>性别</label>
                         </List.Col>
-                        <List.Col tail>
+                        <List.Col>
                             <Radio label="男" name="sex" />
                             <Radio label="女" name="sex" />
                         </List.Col>
@@ -125,6 +136,10 @@ export default class star extends Component{
                             <Switch />
                         </List.Col>
                     </List.Item>
+                </List>
+                <Code target="list-form" />
+
+                <List>
                     <List.Item navigate>
                         <List.Col heading>
                             <label>信息数量</label>
@@ -142,9 +157,9 @@ export default class star extends Component{
                         </List.Col>
                     </List.Item>
                 </List>
+                <Code target="list-badge" />
 
                 <List>
-                    <List.Header>列表标题</List.Header>
                     <List.Item navigate>
                         <List.Col heading>
                             <label>性别</label>
@@ -154,15 +169,10 @@ export default class star extends Component{
                             <Input type="text" placeholder="男/女" disabled />
                         </List.Col>
                     </List.Item>
-                    <List.Item navigate>
-                        <List.Col heading>
-                            <label>性别</label>
-                            <label className="ph-sub-heading">只能选择一次</label>
-                        </List.Col>
-                        <List.Col tail>
-                            <Input type="text" value="男" disabled />
-                        </List.Col>
-                    </List.Item>
+                </List>
+                <Code target="list-heading-twoline" />
+
+                <List>
                     <List.Item navigate>
                         <List.Col heading>
                             <label>性别</label>
@@ -172,35 +182,20 @@ export default class star extends Component{
                             <label className="ph-sub-heading">只能选择一次</label>
                         </List.Col>
                     </List.Item>
+                </List>
+                <Code target="list-tail-twoline" />
+
+                <List>
                     <List.Item navigate>
                         <List.Col heading>
                             <label>性别</label>
                         </List.Col>
                         <List.Col tail>
-                            <Input type="text" value="男" disabled />
-                            <label className="ph-sub-heading">只能选择一次</label>
+                            <Image src={IMAGE_URL} />
                         </List.Col>
                     </List.Item>
                 </List>
-
-                <List>
-                    <List.Header>点击态</List.Header>
-                    <List.Item className="clickable">
-                        <List.Col>
-                            <label>选择一</label>
-                        </List.Col>
-                    </List.Item>
-                    <List.Item className="clickable">
-                        <List.Col>
-                            <label>选择二</label>
-                        </List.Col>
-                    </List.Item>
-                    <List.Item className="clickable">
-                        <List.Col>
-                            <label>选择三</label>
-                        </List.Col>
-                    </List.Item>
-                </List>
+                <Code target="list-image" />
             </div>
         );
     }
